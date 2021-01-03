514cb795a74d57b09f083c9630a86d2c6dbca80a
machadomatt <matheusmacsyn@gmail.com>
2021-01-03 01:16:04 -0300

    Add image alt and disable eslint href warning

--- src/components/Promotion/index.js
+++ src/components/Promotion/index.js
@@ -3,14 +3,16 @@ import './styles.css'
 function Promotion(props) {
   return (
     <div className='promotion'>
-      <img src={props.heroImageUrl} alt='' />
+      <img src={props.heroImageUrl} alt={props.name} />
       <div className='info'>
         <h1>{props.name}</h1>
         <p>{props.description}</p>
         <div className='buttons'>
+          {/* eslint-disable-next-line */}
           <a className='terms' href=''>
             {props.termsAndConditionsButtonText}
           </a>
+          {/* eslint-disable-next-line */}
           <a className='join' href=''>
             {props.joinNowButtonText}
           </a>
