a70081960f963c937f7830b2b998532bf8826dc8
machadomatt <matheusmacsyn@gmail.com>
2021-01-03 01:18:20 -0300

    Changes and new feature
    - Change initialState for data preventing error when first iterate
    - Add new customers promotions filter
    - Use arrow funtions for setData
    - Add key in component map

--- src/App.js
+++ src/App.js
@@ -3,31 +3,53 @@ import './App.css'
 import { getData } from './services/requests'
 import Promotion from './components/Promotion'
 
-function App() {
-  const [data, setData] = useState(null)
+export default function App() {
+  const [data, setData] = useState([])
+  const [filter, setFilter] = useState('all')
 
   useEffect(() => {
+    console.log('dataEffect')
     getData().then(res => {
-      setData(res.data)
+      setData(prevData => (prevData = res.data))
     })
   }, [])
+
+  useEffect(() => {
+    console.log('filterEffect')
+    if (filter === 'new') {
+      setData(prevData => (prevData = prevData.filter(promotion => promotion.onlyNewCustomers)))
+    } else {
+      getData().then(res => {
+        setData(prevData => (prevData = res.data))
+      })
+    }
+  }, [filter])
+
+  console.log('render')
+  console.log(filter)
   console.log(data)
+
   return (
     <div className='App'>
-      {data.map(promotion => (
-        <Promotion
-          id={promotion.id}
-          name={promotion.name}
-          description={promotion.description}
-          heroImageUrl={promotion.heroImageUrl}
-          onlyNewCustomers={promotion.onlyNewCustomers}
-          termsAndConditionsButtonText={promotion.termsAndConditionsButtonText}
-          joinNowButtonText={promotion.joinNowButtonText}
-          sequence={promotion.sequence}
-        />
-      ))}
+      <div className='nav'>
+        <button onClick={() => setFilter('all')}>Promotions</button>
+        <button onClick={() => setFilter('new')}>New Customers</button>
+      </div>
+      <div className='promotions'>
+        {data.map(promotion => (
+          <Promotion
+            key={promotion.id}
+            id={promotion.id}
+            name={promotion.name}
+            description={promotion.description}
+            heroImageUrl={promotion.heroImageUrl}
+            onlyNewCustomers={promotion.onlyNewCustomers}
+            termsAndConditionsButtonText={promotion.termsAndConditionsButtonText}
+            joinNowButtonText={promotion.joinNowButtonText}
+            sequence={promotion.sequence}
+          />
+        ))}
+      </div>
     </div>
   )
 }
-
-export default App
